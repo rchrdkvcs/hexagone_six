d3340f5870971bb6d840c2de84d1eefe241343e1
rchrdkvcs <b34r.rk@gmail.com>
2025-03-09 14:15:02 +0100

    chore(layout): correction de la gestion des layout causant un bug en prod

--- inertia/app/app.ts
+++ inertia/app/app.ts
@@ -1,12 +1,12 @@
 /// <reference path="../../adonisrc.ts" />
 /// <reference path="../../config/inertia.ts" />
 
-import type { DefineComponent } from 'vue'
-import { createApp, h } from 'vue'
+import { createApp, DefineComponent, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
-import DefaultLayout from '~/layouts/DefaultLayout.vue'
+import '~/styles/fonts.css'
 import 'virtual:uno.css'
-import '../styles/fonts.css'
+import { resolvePageComponent } from '@adonisjs/inertia/helpers'
+import DefaultLayout from '~/layouts/DefaultLayout.vue'
 
 createInertiaApp({
   progress: { color: '#00ffe5' },
@@ -14,10 +14,15 @@ createInertiaApp({
   title: (title) => `R6Calls | ${title}`,
 
   resolve: (name) => {
-    const pages = import.meta.glob<DefineComponent>('../pages/**/*.vue', { eager: true })
-    let page = pages[`../pages/${name}.vue`]
-    page.default.layout = page.default.layout || DefaultLayout
-    return page
+    return resolvePageComponent(
+      `../pages/${name}.vue`,
+      import.meta.glob<DefineComponent>('../pages/**/*.vue')
+    ).then((page) => {
+      if (!page.default.layout) {
+        page.default.layout = DefaultLayout
+      }
+      return page
+    })
   },
 
   setup({ el, App, props, plugin }) {
