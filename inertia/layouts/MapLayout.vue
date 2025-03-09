dbca5cf401bac5d8e63b65bc060205bc05438e6f
rchrdkvcs <b34r.rk@gmail.com>
2025-03-09 14:26:16 +0100

    fix(layout): fixed button hover effects in MapLayout component

--- inertia/layouts/MapLayout.vue
+++ inertia/layouts/MapLayout.vue
@@ -29,8 +29,11 @@ const loadImage = (index: number) => {
         <ul slot="nav" class="h-full flex justify-center items-center">
           <li v-for="index in totalImages" :key="index" class="h-full">
             <button
-              :class="{ 'before:(h-5px)': currentImageIndex === index }"
-              class="relative px-8 flex justify-center items-center h-full group before:(content-[''] w-full h-0 hover:h-5px bg-#00ffe5 bottom-0 left-0 absolute transition-all ease-in-out duration-300) after:(content-[''] w-full h-0 bg-gradient-to-t from-#00ffe5/75 to-#00ffe5/0 hover:h-1/2 bottom-0 left-0 absolute transition-all ease-in-out duration-300)"
+              :class="{
+                'before:h-5px': currentImageIndex === index,
+                'before:h-0': currentImageIndex !== index,
+              }"
+              class="relative px-8 flex justify-center items-center h-full group before:(content-[''] w-full hover:h-5px bg-#00ffe5 bottom-0 left-0 absolute transition-all ease-in-out duration-300) after:(content-[''] w-full h-0 bg-gradient-to-t from-[#00ffe5] to-transparent hover:h-1/2 bottom-0 left-0 absolute transition-all ease-in-out duration-300)"
               @click="loadImage(index)"
             >
               <span class="uppercase text-white text-xl font-bold">Etage {{ index }}</span>
