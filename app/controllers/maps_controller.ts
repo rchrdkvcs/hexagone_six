87d190b9c81f02bc44ca536e3adf6a828524de49
rchrdkvcs <b34r.rk@gmail.com>
2025-03-25 19:53:29 +0100

    fix(controller): update MapsController to use 'slug' for map retrieval and pass maps to index view

--- app/controllers/maps_controller.ts
+++ app/controllers/maps_controller.ts
@@ -1,11 +1,16 @@
 import type { HttpContext } from '@adonisjs/core/http'
+import Map from '#models/map'
 
 export default class MapsController {
   index({ inertia }: HttpContext) {
-    return inertia.render('maps/index')
+    const maps = Map.all()
+
+    return inertia.render('maps/index', { maps })
   }
 
-  show({ inertia, params }: HttpContext) {
-    return inertia.render('maps/show', { id: params.id })
+  async show({ inertia, params }: HttpContext) {
+    const map = await Map.query().where('slug', params.slug).preload('markers')
+
+    return inertia.render('maps/show', { map })
   }
 }
