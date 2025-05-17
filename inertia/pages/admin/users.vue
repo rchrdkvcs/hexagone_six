a4a393f2799fe8c30cf3f0431e71aaa9bea06528
rchrdkvcs <b34r.rk@gmail.com>
2025-05-17 13:37:43 +0200

    feat(users): add user table to display name, email, and role

--- inertia/pages/admin/users.vue
+++ inertia/pages/admin/users.vue
@@ -10,5 +10,23 @@ defineProps<{
 <template>
   <div class="size-full p-6">
     <h1 class="text-2xl font-bold">Utilisateurs</h1>
+    <div class="mt-4">
+      <table class="w-full border-collapse border border-gray-300">
+        <thead>
+          <tr>
+            <th class="border border-gray-300 p-2">Nom</th>
+            <th class="border border-gray-300 p-2">Email</th>
+            <th class="border border-gray-300 p-2">Rôle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="user in users" :key="user.id">
+            <td class="border border-gray-300 p-2">{{ user.name }}</td>
+            <td class="border border-gray-300 p-2">{{ user.email }}</td>
+            <td class="border border-gray-300 p-2">{{ user.role }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
