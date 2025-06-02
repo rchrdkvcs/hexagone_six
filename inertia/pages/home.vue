9210a242882cce1c0009df7cc016adebf9cb9dcc
Richard Kovacs <rkovacs@leroylogistique.com>
2025-06-02 17:11:40 +0200

    feat(branding): update branding elements and enhance UI components for improved user experience

--- inertia/pages/home.vue
+++ inertia/pages/home.vue
@@ -1,7 +1,6 @@
 <script lang="ts" setup>
 import HomeHero from '~/components/home/HomeHero.vue'
 import HomeFeatures from '~/components/home/HomeFeatures.vue'
-import HomeFaq from '~/components/home/HomeFaq.vue'
 import HomeCta from '~/components/home/HomeCta.vue'
 </script>
 
@@ -9,7 +8,6 @@ import HomeCta from '~/components/home/HomeCta.vue'
   <div class="space-y-8 md:space-y-24">
     <HomeHero />
     <HomeFeatures />
-    <HomeFaq />
     <HomeCta />
   </div>
 </template>
