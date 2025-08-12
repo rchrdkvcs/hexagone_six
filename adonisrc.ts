a12ced73d48253d08086d06455bf5520bc3957b5
rchrdkvcs <b34r.rk@gmail.com>
2025-08-12 23:48:14 +0200

    feat(statif): corrected statif file serve from public folder

--- adonisrc.ts
+++ adonisrc.ts
@@ -10,7 +10,11 @@ export default defineConfig({
   | will be scanned automatically from the "./commands" directory.
   |
   */
-  commands: [() => import('@adonisjs/core/commands'), () => import('@adonisjs/lucid/commands'), () => import('adonisjs-scheduler/commands')],
+  commands: [
+    () => import('@adonisjs/core/commands'),
+    () => import('@adonisjs/lucid/commands'),
+    () => import('adonisjs-scheduler/commands'),
+  ],
 
   /*
   |--------------------------------------------------------------------------
