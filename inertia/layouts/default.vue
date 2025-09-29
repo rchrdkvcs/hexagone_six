396d5c2029f02f077e46502ac2adada123dd990b
Richard Kovacs <54708737+rchrdkvcs@users.noreply.github.com>
2025-09-29 14:41:04 +0200

    feat: removed #SaltyDuels banner

--- inertia/layouts/default.vue
+++ inertia/layouts/default.vue
@@ -5,11 +5,6 @@ import AppFooter from '~/components/utils/AppFooter.vue'
 
 <template>
   <UApp>
-    <UBanner
-      icon="lucide:twitch"
-      title="La grande finale des #SaltyDuelsbyMSI actuellement sur Twitch - Sxnzoo vs Tamime2K."
-      to="https://www.twitch.tv/sixquatre"
-    />
     <AppHeader />
     <div class="min-h-screen">
       <slot />
